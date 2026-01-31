d904a6e3646c5af806349b00b3ff040bfd8fe8d5
cmcoke <minoru.cm@gmail.com>
2026-01-31 14:13:53 -0500

    Implemented the event details page, server actions and caching

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,20 +1,15 @@
-import { notFound } from "next/navigation";
-
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+import { Suspense } from "react";
+import EventDetails from "@/components/EventDetails";
 
 const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const slug = params.then(p => p.slug);
-  const request = await fetch(`${BASE_URL}/api/events/${slug}`);
-  const data = await request.json();
-
-  if (!data) return notFound();
 
   return (
-    <section id="event">
-      <h1>
-        Event Details: <br /> {slug}
-      </h1>
-    </section>
+    <main>
+      <Suspense fallback={<div>Loading...</div>}>
+        <EventDetails params={slug} />
+      </Suspense>
+    </main>
   );
 };
 export default EventDetailsPage;
